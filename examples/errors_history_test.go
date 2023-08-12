c51342aebf3c041e0a0c38730e4d76065292286b
Avinash Sajjanshetty <opensource@avi.im>
2023-08-12 18:53:24 +0530

    Add an example which shows err history

---
+++ examples/errors_history_test.go
@@ -0,0 +1,45 @@
+package retry_test
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/avast/retry-go/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestErrorHistory shows an example of how to get all the previous errors when
+// retry.Do ends in success
+func TestErrorHistory(t *testing.T) {
+	attempts := 3 // server succeeds after 3 attempts
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if attempts > 0 {
+			attempts--
+			w.WriteHeader(http.StatusBadGateway)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer ts.Close()
+	var allErrors []error
+	err := retry.Do(
+		func() error {
+			resp, err := http.Get(ts.URL)
+			if err != nil {
+				return err
+			}
+			defer resp.Body.Close()
+			if resp.StatusCode != 200 {
+				return fmt.Errorf("failed HTTP - %d", resp.StatusCode)
+			}
+			return nil
+		},
+		retry.OnRetry(func(n uint, err error) {
+			allErrors = append(allErrors, err)
+		}),
+	)
+	assert.NoError(t, err)
+	assert.Len(t, allErrors, 3)
+}
